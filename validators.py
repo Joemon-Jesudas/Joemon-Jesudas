1032241cbbdf3f2ee1cd959e1c05707531471bb1
Joemon-Jesudas <85217855+Joemon-Jesudas@users.noreply.github.com>
2025-11-19 16:21:50 +0530

    Add docstring to get_status_style function
    Added docstring to get_status_style function.

--- validators.py
+++ validators.py
@@ -1,4 +1,5 @@
 def get_status_style(status: str) -> str:
+    """Map validation status to CSS class."""
     if status in ["Correct", "Found"]:
         return "validation-correct"
     if status in ["Mismatch", "Not found"]:
