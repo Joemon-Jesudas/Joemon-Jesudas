e6be13bb836909886a8e053b5de4ea2d6662dadf
Joemon-Jesudas <85217855+Joemon-Jesudas@users.noreply.github.com>
2025-11-25 13:16:20 +0530

    Add legal clauses to legal_template.py

---
+++ legal_template.py
@@ -0,0 +1,11 @@
+# legal_template.py
+REFERENCE_LEGAL_CLAUSES = [
+    "The Contractor shall not manage and organize the supply of the Contractual Services on his own responsibility.",
+    "The Parties agree that Contractor's employees shall not be integrated in the work organization of the Customer. The Parties shall take the necessary measures to avoid such an integration, in particular, both Parties shall train their employees to ensure that they are able to comply with the obligations of this Agreement. No internal e-mail address or telephone number shall be assigned to the Contractor or the Contractor’s employees.",
+    "The Contractor’s employees work independently under the exclusive authority of the Contractor. This applies even if, due to the special features of the performance of Contractual Services, a joint team must be formed with employees of the Customer.",
+    "The Contractor’s employees are not subject to any right of instruction of the Customer, the clients of the Customer and his employees, in particular with regard to place, time and content of the Contractual Services or any disciplinary action.",
+    "The selection and classification of the Contractor’s employees is the sole responsibility of the Contractor.",
+    "The Contractor shall determine the working time and workplace of the Contractor's employees on his own responsibility. At the same time, this shall not negatively affect Contractor’s compliance with agreed deadlines for milestones/work results.",
+    "The Parties agree that the Contractor’s employees do not need to notify the Customer in case of illness, holidays or other events for absence from work. The Customer may not demand such notifications from the Contractor’s employees. The organization of any necessary replacement is the exclusive responsibility of the Contractor.",
+    "The Contractor is also obliged to report any violation of this section 2 by the Customer or his personnel immediately in writing (e-mail is sufficient) with a detailed description of the incident to Customers’ Compliance organization (mail to: AllianzTechnology.Compliance@allianz.com).",
+]
